15be7ff9ba0a9b8a9c793f409f7bb4c9d0ed026c
echo <33642992+Fengshaoyi@users.noreply.github.com>
2019-09-20 16:18:59 +0800

    Add files via upload

---
+++ (2)全局API/190903/dirName.js
@@ -0,0 +1,9 @@
+/**
+ * 全局API，直接调用即可
+ */
+console.log("__dirname      " + __dirname);
+console.log("__filename     " + __filename);
+/**
+ * __dirname变量：获取当前文件所在目录的完整目录名      表示当前执行脚本所在的目录
+ * __filename变量：获取当前模块文件的带有完整绝对路径的文件名
+ */
